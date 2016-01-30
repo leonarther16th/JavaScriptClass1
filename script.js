34ae772f5f8b77237457dfb6c84b11dd687ee821
Moe Katib <Moe.Katib@gmail.com>
2016-01-30 14:35:52 -0500

    added the guess game

--- script.js
+++ script.js
@@ -5,5 +5,21 @@ var extraText = '<p>This is extra text from JS</p>';
 elem[0].innerHTML = elem[0].innerHTML + extraText;
 
 
-var randomNumber = Math.random();
-console.log('random number is ' + randomNumber);
+var randomNumber = Math.floor( Math.random() * 6 ) + 1;
+//console.log('random number is ' + randomNumber);
+
+
+var userGuess = prompt('Please guess a number between 1 and 6');
+
+var userGuess = parseInt(userGuess);
+
+var feedHolder = document.getElementById('feedHolder');
+
+if ( userGuess === randomNumber ) {
+	feedHolder.innerHTML = "<p class='right'>Amazing you guessed right! You are a wizard!</p>";
+} else {
+	feedHolder.innerHTML = "<p class='wrong'>Boomer!! You did not guess this time! The right number is " + randomNumber + "</p>";
+}
+
+
+
